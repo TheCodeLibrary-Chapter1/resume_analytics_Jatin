4f4d9a07b4267ab689b37a2c607be94537d91a4b
TheCodeLibrary-Chapter1 <36452292+TheCodeLibrary-Chapter1@users.noreply.github.com>
2021-03-16 17:31:35 +0530

    Update data_parser.py

--- api/data_parser.py
+++ api/data_parser.py
@@ -371,7 +371,7 @@ class resumeparse(object):
                 text += " ".join(values) + " "
             rx = re.compile(r"(\d+(?:-\d+)?\+?)\s*(years?)", re.I)
             op = rx.search(text)
-            print(text)
+
             
             if op!=None:
                 final_exp = [float(i) for i in op.groups() if i.isnumeric()]
